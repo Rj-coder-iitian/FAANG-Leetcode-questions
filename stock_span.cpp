18fefe088a057552d2816c4c2be360d98e4cf9b7
examsrj <91756826+examsrj@users.noreply.github.com>
2021-10-02 00:43:54 +0530

    added code for stock span problem

---
+++ stock_span.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> stockspan(vector<int> prices){
+    vector<int> ans;
+    stack<pair<int,int>> s;
+    for(auto price: prices){
+        int days = 1;
+        while(!s.empty() and s.top().first <= price){
+            days += s.top().second;
+            s.pop();
+        }
+        s.push({price,days});
+        ans.push_back(days);
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cin >>n;
+    vector<int> a(n);
+    for(auto &i : a){
+        cin >> i;
+    }
+    // vector<int> a = {100,80,60,70,60,75,85};
+    vector<int> res = stockspan(a);
+    for(auto i : res){
+        cout << i<<" ";
+    }
+    cout <<"\n";
+    return 0;
+}
