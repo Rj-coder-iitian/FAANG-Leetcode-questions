1756cc5cc6116f046d4e9b7744371149f0e56fb0
Supratim Bhattacharjee <pikucode15@gmail.com>
2021-10-02 00:30:43 +0530

    Count Complete Tree Nodes Tree Problem

---
+++ Count_Complete_Tree_Nodes.cpp
@@ -0,0 +1,44 @@
+//Problem Link:- https://leetcode.com/problems/count-complete-tree-nodes/
+class Solution {
+public:
+    int leftHeight(TreeNode* root)
+    {
+        if(!root)
+            return 0;
+        int count=0;
+        TreeNode* p=root;
+        while(p)
+        {
+            count++;
+            p=p->left;
+        }
+        return count;
+    }
+    
+    int rightHeight(TreeNode* root)
+    {
+        if(!root)
+            return 0;
+        int count=0;
+        TreeNode* p=root;
+        while(p)
+        {
+            count++;
+            p=p->right;
+        }
+        return count;
+    }
+    
+    int helper(TreeNode* root)
+    {
+        if(!root)
+            return 0;
+        int lh=leftHeight(root);
+        int rh=rightHeight(root);
+        return lh==rh?(1<<lh)-1:1+helper(root->left)+helper(root->right);
+    }
+    
+    int countNodes(TreeNode* root) {
+        return helper(root);
+    }
+};
