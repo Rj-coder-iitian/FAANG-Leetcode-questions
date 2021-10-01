8346e3300cc721cf0b5ce95ee5a4b954cc17b8e8
rishabh-esummit <91755818+rishabh-esummit@users.noreply.github.com>
2021-10-02 00:25:55 +0530

    Added trapping rainwater problem

---
+++ trapping_rainwater.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int rain_water(vector<int> a){
+    stack<int> st;
+    int n = a.size(),ans=0;
+    for(int i=0;i<n;i++){
+        while(!st.empty() and a[st.top()] < a[i]){
+            int cur = st.top();
+            st.pop();
+            if(st.empty()){
+                break;
+            }
+            int diff = i - st.top() - 1;
+            ans += (min(a[st.top()],a[i]) - a[cur]) * diff;
+        }
+        st.push(i);
+    }
+    return ans;  
+}   
+int main(){
+    int n;
+    cin >>n;
+    vector<int> a(n); // 0 1 0 2 1 0 1 3 2 1 2 1
+    for(auto &i : a){
+        cin >> i;
+    }
+    cout <<rain_water(a);
+}
