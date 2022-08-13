c39c1ed95959494e3706e102ca8bdcf9eaf4f4e6
Rishabh Jain <55445973+Rj-coder-iitian@users.noreply.github.com>
2022-08-13 16:36:27 +0530

    Time: 105 ms (27.68%), Space: 42.5 MB (21.27%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -9,43 +9,77 @@
  */
 class Codec {
 public:
-
-    // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        ostringstream out;
-        serialize(root, out);
-        return out.str();
-    }
+        if(!root) {
+            return "NULL,";
+        }
+        return to_string(root->val)+","+serialize(root->left)+serialize(root->right);
+    }   
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        istringstream in(data);
-        return deserialize(in);
-    }
-private:
-
-    // Encodes a tree to a single string.
-    void serialize(TreeNode* root, ostringstream& out) {
-        if(root){
-            out << root->val << ' ';
-            serialize(root->left, out);
-            serialize(root->right, out);
-        } else {
-            out << "# ";
+        queue<string> q;
+        string s;
+        for(int i=0;i<data.size();i++)
+        {
+            if(data[i]==',')
+            {
+                q.push(s);
+                s="";
+                continue;
+            }
+            s+=data[i];
         }
+        if(s.size()!=0)q.push(s);
+        return deserialize_helper(q);
     }
 
-    // Decodes your encoded data to tree.
-    TreeNode* deserialize(istringstream& in) {
-        string val;
-        in >> val;
-        if(val == "#")
-            return nullptr;
-        TreeNode* root = new TreeNode(stoi(val));
-        root->left = deserialize(in);
-        root->right = deserialize(in);
+    TreeNode* deserialize_helper(queue<string> &q) {
+        string s=q.front();
+        q.pop();
+        if(s=="NULL")return NULL;
+        TreeNode* root=new TreeNode(stoi(s));
+        root->left=deserialize_helper(q);
+        root->right=deserialize_helper(q);
         return root;
     }
+
+//     // Encodes a tree to a single string.
+//     string serialize(TreeNode* root) {
+//         ostringstream out;
+//         serialize(root, out);
+//         return out.str();
+//     }
+
+//     // Decodes your encoded data to tree.
+//     TreeNode* deserialize(string data) {
+//         istringstream in(data);
+//         return deserialize(in);
+//     }
+// private:
+
+//     // Encodes a tree to a single string.
+//     void serialize(TreeNode* root, ostringstream& out) {
+//         if(root){
+//             out << root->val << ' ';
+//             serialize(root->left, out);
+//             serialize(root->right, out);
+//         } else {
+//             out << "# ";
+//         }
+//     }
+
+//     // Decodes your encoded data to tree.
+//     TreeNode* deserialize(istringstream& in) {
+//         string val;
+//         in >> val;
+//         if(val == "#")
+//             return nullptr;
+//         TreeNode* root = new TreeNode(stoi(val));
+//         root->left = deserialize(in);
+//         root->right = deserialize(in);
+//         return root;
+//     }
 };
 
 // Your Codec object will be instantiated and called as such:
