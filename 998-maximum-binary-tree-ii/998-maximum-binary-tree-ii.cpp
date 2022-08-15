392f6064bf69c5cda4291fdee032218187bb185a
Rishabh Jain <55445973+Rj-coder-iitian@users.noreply.github.com>
2022-08-15 12:19:50 +0530

    Time: 7 ms (64.85%), Space: 13.4 MB (63.27%) - LeetHub

--- 998-maximum-binary-tree-ii/998-maximum-binary-tree-ii.cpp
+++ 998-maximum-binary-tree-ii/998-maximum-binary-tree-ii.cpp
@@ -11,33 +11,42 @@
  */
 class Solution {
 public:
-    void insertIntoMaxTreeArr(TreeNode* root, vector<int>& nums) {
-        if(!root)
-            return;
-        insertIntoMaxTreeArr(root->left, nums);
-        nums.push_back(root->val);
-        insertIntoMaxTreeArr(root->right, nums);
-    }
-    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        int n = nums.size();
-        vector<TreeNode*> stk;
-        for(int i=0;i<n;i++){
-            TreeNode* curr = new TreeNode(nums[i]);
-            while(!stk.empty() && stk.back()->val < nums[i]) {
-                curr->left = stk.back();
-                stk.pop_back();
-            }
-            if(!stk.empty()) {
-                stk.back()->right = curr;
-            }
-            stk.push_back(curr);
-        }
-        return stk.front();
-    }
     TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
-        vector<int> nums;
-        insertIntoMaxTreeArr(root, nums);
-        nums.push_back(val);
-        return constructMaximumBinaryTree(nums);
+        if(root && root->val > val){
+            root->right = insertIntoMaxTree(root->right, val);
+            return root;
+        }
+        TreeNode* node = new TreeNode(val);
+        node->left = root;
+        return node;
     }
+    // void insertIntoMaxTreeArr(TreeNode* root, vector<int>& nums) {
+    //     if(!root)
+    //         return;
+    //     insertIntoMaxTreeArr(root->left, nums);
+    //     nums.push_back(root->val);
+    //     insertIntoMaxTreeArr(root->right, nums);
+    // }
+    // TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<TreeNode*> stk;
+    //     for(int i=0;i<n;i++){
+    //         TreeNode* curr = new TreeNode(nums[i]);
+    //         while(!stk.empty() && stk.back()->val < nums[i]) {
+    //             curr->left = stk.back();
+    //             stk.pop_back();
+    //         }
+    //         if(!stk.empty()) {
+    //             stk.back()->right = curr;
+    //         }
+    //         stk.push_back(curr);
+    //     }
+    //     return stk.front();
+    // }
+    // TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+    //     vector<int> nums;
+    //     insertIntoMaxTreeArr(root, nums);
+    //     nums.push_back(val);
+    //     return constructMaximumBinaryTree(nums);
+    // }
 };
