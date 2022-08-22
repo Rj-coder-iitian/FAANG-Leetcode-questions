d349bf55f3d80b45dde256bd780ae5b8956aa107
Rishabh Jain <55445973+Rj-coder-iitian@users.noreply.github.com>
2022-08-22 21:31:51 +0530

    Time: 905 ms (13.67%), Space: 173.3 MB (9.05%) - LeetHub

---
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -0,0 +1,64 @@
+class NumArray {
+public:
+    vector<int> seg;
+    int n;
+    void build(int ind, int low, int high, vector<int>& arr) {
+        if(low == high) {
+            seg[ind] = arr[low];
+            return;
+        }
+        int mid = (low + high) / 2;
+        build(2*ind + 1, low, mid, arr);
+        build(2*ind + 2, mid + 1, high, arr);
+        seg[ind] = seg[2*ind+1] + seg[2*ind+2];
+    }
+    int query(int ind, int low, int high, int l, int r) {
+        //no overlap
+        //l r low high || low high l r
+        if(r < low || l > high) {
+            return 0;
+        }
+        //complete overlap
+        //{l low high r}
+        if(low >= l && high <= r) {
+            return seg[ind];
+        }
+        int mid = (low + high) >> 1;
+        int left = query(2*ind + 1, low, mid, l, r);
+        int right = query(2*ind + 2, mid + 1, high, l, r);
+        return left + right;
+    }
+    void update(int ind, int low, int high, int i, int val) {
+        if(low == high){
+            seg[ind] = val;
+            return;
+        }
+        int mid = (low + high) >> 1;
+        if(i <= mid) {
+            update(2*ind + 1, low, mid, i, val);
+        } else {
+            update(2*ind + 2, mid + 1, high, i, val);
+        }
+        seg[ind] = seg[2*ind + 1] + seg[2*ind + 2];
+    }
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        seg.reserve(4 * n + 1);
+        build(0, 0, n-1, nums);
+    }
+    
+    void update(int index, int val) {
+        update(0, 0, n-1, index, val);
+    }
+    
+    int sumRange(int left, int right) {
+        return query(0, 0, n-1, left, right);
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
