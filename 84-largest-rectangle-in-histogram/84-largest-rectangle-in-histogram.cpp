4ae5decf28597dc9b5c5889dec233fce3e2408cb
Rishabh Jain <55445973+Rj-coder-iitian@users.noreply.github.com>
2022-09-27 16:57:35 +0530

    Time: 415 ms (22.00%), Space: 77.2 MB (67.49%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,49 +1,24 @@
 class Solution {
 public:
-    vector<int> nextSmallerRight(vector<int>& heights, int n) {
-        vector<int> res(n);
-        stack<int> st;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && heights[st.top()] >= heights[i])
-                st.pop();
-            if(st.empty())
-                res[i] = -1;
-            else 
-                res[i] = st.top();
-            st.push(i);
-        }
-        return res;
-    }
-    
-    vector<int> nextSmallerLeft(vector<int>& heights, int n) {
-        vector<int> res(n);
-        stack<int> st;
-        for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()] >= heights[i])
-                st.pop();
-            if(st.empty())
-                res[i] = -1;
-            else 
-                res[i] = st.top();
-            st.push(i);
-        }
-        return res;
-    }
-    
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> leftSmaller = nextSmallerLeft(heights, n);
-        vector<int> rightSmaller = nextSmallerRight(heights, n);
-        int res = INT_MIN;
-        for(int i=0;i<n;i++){
-            int left = leftSmaller[i];
-            int right = rightSmaller[i];
-            if(left == -1) left = -1;
-            if(right == -1) right = n;
-            
-            int w = (right - 1) - (left + 1) + 1;
-            res = max(res, heights[i]*w);
-        }
-        return res;
+	
+		int n = heights.size();
+		if (!n) return 0;
+        
+        heights.push_back(0);
+        stack<int>stk;
+		int res = 0;
+		
+		for (int i = 0; i < heights.size(); i++) {
+			while (!stk.empty() && heights[stk.top()] > heights[i]) {
+                int h = heights[stk.top()];
+                stk.pop();
+                int l = stk.empty() ? 0 : stk.top()+1;
+                res = max(res, (i-l)*h);
+			}
+			stk.push(i);
+		}
+        
+		return res;
     }
 };
