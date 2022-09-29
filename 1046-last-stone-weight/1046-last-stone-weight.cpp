15e97e77f92b1947557457620c09bae6437e6328
Rishabh Jain <55445973+Rj-coder-iitian@users.noreply.github.com>
2022-09-29 16:50:01 +0530

    Time: 4 ms (40.94%), Space: 7.7 MB (31.89%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,24 +1,13 @@
 class Solution {
 typedef pair<int, int> pi;
 public:
-    int lastStoneWeight(vector<int>& stones) {
-        priority_queue<pi> pq;
-        for(int i=0;i<stones.size();i++) {
-            pq.push(make_pair(stones[i], i));
+    int lastStoneWeight(vector<int>& A) {
+        priority_queue<int> pq(begin(A), end(A));
+        while (pq.size() > 1) {
+            int x = pq.top(); pq.pop();
+            int y = pq.top(); pq.pop();
+            if (x > y) pq.push(x - y);
         }
-        while(pq.size() > 1){
-            auto [a_num, a_i] = pq.top();
-            pq.pop();
-            auto [b_num, b_i] = pq.top();
-            pq.pop();
-            if(a_num == b_num)
-                continue;
-            if(a_num > b_num){
-                pq.push(make_pair(a_num - b_num, a_i));
-            }else {
-                pq.push(make_pair(b_num - a_num, b_i));
-            }
-        }
-        return pq.size() > 0 ? pq.top().first : 0;
+        return pq.empty() ? 0 : pq.top();
     }
 };
